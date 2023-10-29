5418167cf369b1e23d2036cf8d2a3ce7eaacf63c
InnovatorNZ's Ryzen 9 Desktop <ni33271@live.com>
2023-10-29 16:47:51 +0800

    GCParameter uses constexpr instead of const as its propable undefined initialization.

--- GCParameter.h
+++ GCParameter.h
@@ -27,11 +27,11 @@ public:
 	static constexpr bool recordNewMemMap = false;				// 是否在分配新内存时记录其起始位置和大小，用于二级内存池释放预留内存用，没什么用，不建议启用；前提条件：启用二级内存分配器，启用释放预留内存
 	static constexpr bool bitmapMemoryFromSecondary = true;		// 位图的内存是否从二级分配器分配；启用该选项可以加快位图的内存分配，但可能会导致二级分配器的内存碎片；前提条件：启用二级内存分配器
 	static constexpr bool useGCPtrSet = false;					// 是否启用记录所有GCPtr的集合，若你的程序由于未对新分配内存进行初始化而在GC时崩溃可启用该选项，请谨慎这会导致较大的性能下降；前提条件：启用析构函数
-	static const size_t secondaryMallocSize = 8 * 1024 * 1024;		// 二级内存分配器单次向操作系统请求分配预留内存的大小（默认：8MB）
-	static const size_t TINY_OBJECT_THRESHOLD = 24;					// 迷你对象的对象大小上限（默认：24字节）
-	static const size_t TINY_REGION_SIZE = 256 * 1024;				// 迷你对象的区域大小（默认：256KB）
-	static const size_t SMALL_OBJECT_THRESHOLD = 16 * 1024;			// 小对象的对象大小上限（默认：16KB）
-	static const size_t SMALL_REGION_SIZE = 2 * 1024 * 1024;		// 小对象的区域大小（默认：2MB）
-	static const size_t MEDIUM_OBJECT_THRESHOLD = 1 * 1024 * 1024;	// 中对象的对象大小上限（默认：1MB）
-	static const size_t MEDIUM_REGION_SIZE = 32 * 1024 * 1024;		// 中对象的区域大小（默认：32MB）
+	static constexpr size_t secondaryMallocSize = 8 * 1024 * 1024;		// 二级内存分配器单次向操作系统请求分配预留内存的大小（默认：8MB）
+	static constexpr size_t TINY_OBJECT_THRESHOLD = 24;					// 迷你对象的对象大小上限（默认：24字节）
+	static constexpr size_t TINY_REGION_SIZE = 256 * 1024;				// 迷你对象的区域大小（默认：256KB）
+	static constexpr size_t SMALL_OBJECT_THRESHOLD = 16 * 1024;			// 小对象的对象大小上限（默认：16KB）
+	static constexpr size_t SMALL_REGION_SIZE = 2 * 1024 * 1024;		// 小对象的区域大小（默认：2MB）
+	static constexpr size_t MEDIUM_OBJECT_THRESHOLD = 1 * 1024 * 1024;	// 中对象的对象大小上限（默认：1MB）
+	static constexpr size_t MEDIUM_REGION_SIZE = 32 * 1024 * 1024;		// 中对象的区域大小（默认：32MB）
 };
